d13e789b7af261f2944c854ae193c79e4b1e94f5
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-11-28 15:45:13 +0100

    fixing task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        i = i + 32
-    print("{}".format(chr(i)), end='')
+        print("{}".format(chr(i - 32)), end='')
+    else:
+        print("{}".format(chr(i)), end='')
