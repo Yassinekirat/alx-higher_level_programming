1da0d91a26062e9b28e3cc9664bfe2d066edbf8f
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-11-28 15:22:33 +0100

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            uppercase_result = chr(ord(char) - 32)
+        else:
+            uppercase_result = char
+        print("{}".format(uppercase_result), end="")
+    print()
