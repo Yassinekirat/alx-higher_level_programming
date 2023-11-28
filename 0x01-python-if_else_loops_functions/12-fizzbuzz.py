fd15005bdcee9030a0e483f31e65af247c8c17a1
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-11-28 16:12:57 +0100

    removing the new line

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(1, 101):
-    if num % 15 == 0:
+    if num % 3 == 0 and num % 5 == 0:
         print("FizzBuzz ", end=" ")
     elif num % 3 == 0:
         print("Fizz ", end=" ")
@@ -8,4 +8,3 @@ for num in range(1, 101):
         print("Buzz ", end=" ")
     else:
         print("{}".format(num), end=" ")
-print()
