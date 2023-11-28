3e357e97e04e6a0d06306127900ee10976ca8817
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-11-28 15:09:46 +0100

    task 6 final fix pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i != j:
-            print("{}{}".format(i, j), end=', ' if not (i == 8 and j == 9) else '')
+            end_str = ', ' if not (i == 8 and j == 9) else ''
+            print("{}{}".format(i, j), end=end_str)
 print()
