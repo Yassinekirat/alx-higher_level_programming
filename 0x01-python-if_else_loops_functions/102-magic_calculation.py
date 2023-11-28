ad1bc50f3405e65ba4bd1bd8f1baa9b13fbd1ad9
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-11-28 15:57:24 +0100

    last task hopefully finished

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    elif c > b:
+        res1 = a + b
+        return (res1)
+    else:
+        res2 = a * b - c
+        return (res2)
