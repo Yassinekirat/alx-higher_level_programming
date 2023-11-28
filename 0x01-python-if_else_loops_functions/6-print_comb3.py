4273630433dbcd75610231a7331fa6f80357c148
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-11-28 15:03:48 +0100

    still fixing task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(10):
-        print("{}{}".format(i, j), end=", ")
+    for j in range(i + 1, 10):
+        if i != j:
+            print("{}{}".format(i, j), end=', ')
 print()
