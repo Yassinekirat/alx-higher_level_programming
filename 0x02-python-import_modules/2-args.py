44bdd0b18f9ffc033949d794efb0c333c4d696a0
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-11-30 12:20:32 +0100

    testing task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    num_args = len(sys.argv) - 1
+
+    if num_args > 1:
+        print("{} arguments:".format(num_args))
+        count = 1
+        for arg in sys.argv[1:]:
+            print("{}: {}".format(count, arg))
+            count += 1
+    elif num_args == 1:
+        print("{} argument:".format(num_args))
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments.".format(num_args))
