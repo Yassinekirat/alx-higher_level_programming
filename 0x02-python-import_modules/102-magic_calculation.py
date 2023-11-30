66f1f6fae7f1db10acbe8328e9710505d8cd7d2b
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-11-30 14:16:15 +0100

    task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+def magic_calculation(x, y):
+    if x < y:
+        result = add(x, y)
+        for i in range(4, 6):
+            result = add(result, i)
+        return result
+    else:
+        return sub(x, y)
+    
