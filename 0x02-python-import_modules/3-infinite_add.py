ca11afce3d6329e50afaca2f53a98ad8ac131aa3
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-11-30 12:32:24 +0100

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) <= 1:
+        print("0")
+    else:
+        total_sum = 0
+        for i in range(1, len(sys.argv)):
+            total_sum += int(sys.argv[i])
+        print(total_sum)
