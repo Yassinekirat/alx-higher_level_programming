eb1c2fba8bc139edb6a856c48cd26717923e5a52
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-12-04 15:29:51 +0100

    task 2 retrieving an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def element_at(my_list, idx):
+    if int(idx) < 0:
+        return None
+    if int(idx) >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
