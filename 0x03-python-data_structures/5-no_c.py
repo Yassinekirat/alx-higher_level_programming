01a93d83072daaa01b1c87fbdd033a4ed03fb0f3
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-12-04 16:10:09 +0100

    task 5 is no C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    result_string = ''
+    for character in my_string:
+        if character != 'c' and character != 'C':
+            result_string += character
+    return result_string
