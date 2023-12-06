51c1630ad53574e5b8e597b928511ccdca91e415
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-12-06 12:43:09 +0100

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = a_dictionary.copy(); 
+    new_dictionary = a_dictionary.copy();
     for key in new_dictionary: new_dictionary[key] *= 2;
     return new_dictionary
