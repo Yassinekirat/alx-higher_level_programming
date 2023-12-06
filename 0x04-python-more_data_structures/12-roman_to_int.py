b6a4e6f619e34df33f6801d5c411b34752ebe920
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-12-06 12:26:34 +0100

    betty

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,13 +6,12 @@ def roman_to_int(roman_string):
 
     for i in range(len(roman_string) - 1):
         current_value = roman_letters[roman_string[i]]
-        next_value = roman_letters[roman_string[i + 1]]
-        
+        next_value = roman_letters[roman_string[i + 1]]    
         if current_value < next_value:
             result -= current_value
         else:
             result += current_value
-    
+
     result += roman_letters[roman_string[-1]]
-    
+
     return result
