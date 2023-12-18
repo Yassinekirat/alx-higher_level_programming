b38773f17fd87dc9eba13317540769a0ef454eae
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-12-18 12:08:53 +0100

    Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (TypeError, ValueError, IndexError, ZeroDivisionError) as error:
+        print(f"Exception: {error}", file=sys.stderr)
+        return None
