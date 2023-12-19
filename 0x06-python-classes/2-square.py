f8dc1c419a387d0b9b6827a48952217a476ee28e
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-12-19 14:34:25 +0100

    maybe fixing task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,8 @@
 class Square:
     """constructor size of a square"""
     def __init__(self, size):
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Square class definition for representing a square."""
+
+
+class Square:
+    """constructor size of a square"""
+    def __init__(self, size):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size ** 2)
