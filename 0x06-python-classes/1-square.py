a468c26c1b8e6d72493a691820d3b0c65a26d3de
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-12-19 14:29:41 +0100

    task 1 Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Square class definition for representing a square."""
+
+
+class Square:
+    """constructor size of a square"""
+    def __init__(self, size):
+        self.__size = size
