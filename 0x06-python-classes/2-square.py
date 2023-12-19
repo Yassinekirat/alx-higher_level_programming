8c9d46a81c412eb3f7edc3d5057a86dc1631ab7c
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-12-19 14:32:01 +0100

    task 2 Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Square class definition for representing a square."""
+
+
+class Square:
+    """constructor size of a square"""
+    def __init__(self, size):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
