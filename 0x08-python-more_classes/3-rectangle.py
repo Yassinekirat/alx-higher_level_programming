cce33d7ddd8e75397d3d90d3182dcf217f3d0455
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-04 10:32:59 +0100

    my bad

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,7 +50,7 @@ class Rectangle:
 
     def __str__(self) -> str:
         """Return the rectangle"""
-        if self.perimeter == 0:
+        if self.width == 0 or self.height == 0:
             return ""
 
         string = ""
