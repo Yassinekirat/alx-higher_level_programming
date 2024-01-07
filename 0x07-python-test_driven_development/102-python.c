2c5b714b6302691f71bcaff893a0bd5205e4ed60
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-07 12:21:44 +0100

    all remaining tasks

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,13 @@
+/*
+ * File: 102-python.c
+ */
+
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+}
