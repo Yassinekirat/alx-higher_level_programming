027cfddd4e84889311dc906db71b6598877903b8
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-09 00:53:45 +0100

    final task

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""add_attribute function"""
+
+def add_attribute(obj, name, value):
+    """Add new attribute."""
+    if hasattr(obj, name):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
