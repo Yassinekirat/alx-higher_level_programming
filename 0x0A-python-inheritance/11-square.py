bb5fed02537df33d62742d4fc0ac992046f1ba3b
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-09 00:47:13 +0100

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+
+    def __init__(self, size):
+        """initialisation"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """area"""
+        return (self.__size ** 2)
+
+    def __str__(self):
+        """description"""
+        return (f"[Square] {self.__size}/{self.__size}")
