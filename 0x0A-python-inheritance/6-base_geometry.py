065c2fa190dc22be87300820d85adbe5fb4f0198
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-08 23:54:25 +0100

    task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Class"""
+
+
+class BaseGeometry:
+    """A basic geometry class."""
+
+    def area(self):
+        """Raises an error message."""
+        raise Exception("area() is not implemented")
