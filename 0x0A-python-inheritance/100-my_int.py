295c453f4184b4b405d01d7e4500f2277a28c7a1
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-09 00:48:36 +0100

    task 12 my integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""My integer class"""
+
+
+class MyInt(int):
+    """Subclass of int with inverted equality."""
+
+    def __eq__(self, other):
+        """Override the == operator to invert its behavior."""
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        """Override the != operator to invert its behavior."""
+        return not super().__ne__(other)
