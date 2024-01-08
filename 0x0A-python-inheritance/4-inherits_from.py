9db422b21e1e5499c413ea7ad63d6867ded59952
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-08 23:51:03 +0100

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Check object's inheritance, excluding direct class"""
+
+
+def inherits_from(obj, a_class):
+    """Check object's inheritance, excluding direct class"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
