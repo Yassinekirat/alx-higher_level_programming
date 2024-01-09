3908cec022000e0b51a2c366833ce3b75b1ea3dc
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-09 11:01:20 +0100

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write to a file"""
+
+
+def write_file(filename="", text=""):
+    """Write to a file"""
+    with open(filename, 'w', encoding='UTF-8') as f:
+        return f.write(text)
