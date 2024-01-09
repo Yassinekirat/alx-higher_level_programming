3e66a851a868d6b095ad8e193efb877017bcf614
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-10 00:51:09 +0100

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Define class."""
+
+class Student:
+    """Get dictionary."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize attributes."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get dictionary."""
+        return self.__dict__
