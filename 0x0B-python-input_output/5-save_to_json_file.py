65c9fcc73fde40162d3b75d183a1eb3e69684778
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-09 11:28:01 +0100

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Save Object to a file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Save Object to a file"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(json.dumps(my_obj))
