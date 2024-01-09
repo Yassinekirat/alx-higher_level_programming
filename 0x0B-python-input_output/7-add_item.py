2d0dc6c35406438f12fe183b06c6cb0623365bbe
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-10 00:45:12 +0100

    gg

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,8 +4,8 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    l = __import__('6-load_from_json_file').load_from_json_file
-    load_from_json_file = l
+    load = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = load
 
     try:
         list = load_from_json_file("add_item.json")
