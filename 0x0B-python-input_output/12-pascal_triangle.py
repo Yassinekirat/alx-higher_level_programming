4d4178a27a5073297d395d2348d964d0521a1016
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-10 01:08:51 +0100

    task 12 pascal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Pascal triangle"""
+
+
+def pascal_triangle(n):
+    """Pascal triangle"""
+    list_new = []
+    if n <= 0:
+        return ([])
+    list_new = [[1]]
+    for i in range(n - 1):
+        tmp = [0] + list_new[-1] + [0]
+        row = []
+        for j in range(len(list_new[-1]) + 1):
+            row.append(tmp[j] + tmp[j + 1])
+        list_new.append(row)
+    return (list_new)
