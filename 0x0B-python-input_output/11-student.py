0e9be44aebcab489a33c46d724acce20ce674594
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-10 01:05:03 +0100

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Define class."""
+
+
+class Student:
+    """Get dictionary."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize attributes."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get dictionary"""
+        if attrs and all(isinstance(attr, str) for attr in attrs):
+            return {
+                attr: getattr(self, attr, None)
+                for attr in attrs if hasattr(self, attr)
+            }
+        return {key: getattr(self, key) for key in sorted(self.__dict__)
+                if hasattr(self, key)}
+
+    def reload_from_json(self, json):
+        """Reload from json."""
+        for key, value in json.items():
+            setattr(self, key, getattr(self, key, value))
