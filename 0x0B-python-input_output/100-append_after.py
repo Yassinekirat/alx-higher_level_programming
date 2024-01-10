452ac5c032447151dcebf9ddfc0dbff428f66d7a
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-01-10 01:16:43 +0100

    task 13 Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Search and update"""
+    text = ""
+    with open(filename, "r") as txt:
+        for line in txt:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as file:
+        file.write(text)
