dc75cffcb7311b4af2cb65f17870d7b0ef8b0c2e
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-02-14 19:22:05 +0100

    task 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- script that lists all shows contained in the database hbtn_0d_tvshows
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities 
+(
+	id INT NOT NULL AUTO_INCREMENT, -- Auto-incrementing ID
+	state_id INT NOT NULL, -- Generic state ID
+	name VARCHAR(256) NOT NULL, -- City name
+	PRIMARY KEY (id), -- Set the primary key to 'id'
+    FOREIGN KEY (state_id) REFERENCES states (id)
+);
