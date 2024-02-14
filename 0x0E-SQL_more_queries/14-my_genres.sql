aa5fd176c1759ecb34d8e8a622a47e05230383af
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-02-14 20:51:12 +0100

    14. My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,1 +1,8 @@
 --
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY tv_genres.name
+ORDER BY tv_genres.name ASC;
