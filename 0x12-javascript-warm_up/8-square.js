c29d511bc390f7f4223c971ec368ea5432cf24bc
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-03-11 14:27:10 +0000

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const size = parseInt(process.argv[2]);
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
