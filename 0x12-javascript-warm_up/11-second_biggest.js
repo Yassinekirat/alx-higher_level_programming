504329867cb412c3410ccddc9853d474cb3a547c
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-03-11 15:21:11 +0000

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+if ((isNaN(process.argv[2])) || (process.argv.length === 3)) {
+  console.log('0');
+} else {
+  const integers = process.argv.slice(2).map(Number).filter(Number.isInteger).sort((a, b) => a - b);
+  console.log(integers[integers.length - 2]);
+}
