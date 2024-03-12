17e7ccbb94063d31cec3d209c252f0f1b4a4b68c
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-03-12 13:02:17 +0000

    task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+const newDict = {};
+
+for (const key in dict) {
+  if (Object.prototype.hasOwnProperty.call(dict, key)) {
+    const value = dict[key];
+    newDict[value] = newDict[value] || [];
+    newDict[value].push(key);
+  }
+}
+
+console.log(newDict);
