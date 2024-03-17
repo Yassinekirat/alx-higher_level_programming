59e5eb4425cd9625f3c4e4f326ce1a1405966d1b
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-03-17 12:51:14 +0000

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Lists states matching a given name"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
+    db_connection = MySQLdb.connect(user=username, passwd=password,
+                                    db=database_name, host='localhost',
+                                    port=3306)
+
+    cursor = db_connection.cursor()
+    query = """SELECT cities.id, cities.name, states.name
+                    FROM cities INNER JOIN states
+                    ON cities.state_id = states.id"""
+    cursor.execute(query)
+    matching_states = cursor.fetchall()
+
+    for row in matching_states:
+        print(row)
+
+    db_connection.close()
