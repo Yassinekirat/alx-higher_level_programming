e8639a84ca6643c3521e0a96af596b6aa05ff8b4
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-03-17 12:24:32 +0000

    fix

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists states"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
+    db_connection = MySQLdb.connect(user=username, passwd=password,
+                                    db=database_name, host='localhost',
+                                    port=3306)
+
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    all_rows = cursor.fetchall()
+
+    for row in all_rows:
+        print(row)
+
+    db_connection.close()
