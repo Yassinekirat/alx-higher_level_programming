833b2506e739fbc9e36faed49c5c60f22d43faf9
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-03-17 12:29:52 +0000

    fix pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,16 +8,18 @@ if __name__ == "__main__":
     username = argv[1]
     password = argv[2]
     database_name = argv[3]
-    
+
     db_connection = MySQLdb.connect(user=username, passwd=password,
-                                     db=database_name, host='localhost', port=3306)
-    
+                                    db=database_name, host='localhost',
+                                    port=3306)
+
     cursor = db_connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    cursor.execute(query)
     n_starting_states = cursor.fetchall()
-    
+
     for row in n_starting_states:
         print(row)
 
-    cursor.close()    
+    cursor.close()
     db_connection.close()
