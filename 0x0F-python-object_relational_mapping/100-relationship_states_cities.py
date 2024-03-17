46605ccb306b00c558b4713f8333def9b6874507
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-03-17 14:34:33 +0000

    task 15. City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""List all states"""
+import sys
+from relationship_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    session.add(City(name="San Francisco", state=State(name="California")))
+    session.commit()
+    session.close()
