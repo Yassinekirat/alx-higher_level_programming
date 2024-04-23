a6ebfd7315494b570d687bdadc3f5ec97eeced9c
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-23 21:52:24 +0100

    humm

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,7 +7,7 @@ request.get(url + process.argv[2], function (error, res, body) {
   if (error) {
     console.log(error);
   } else {
-    let json = JSON.parse(body);
-    console.log(JSON.parse(body).title);
+    const json = JSON.parse(body);
+    console.log(json.title);
   }
 });
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+// Prints the number of movies where "Wedge Antilles" is present
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  
+  if (res.statusCode !== 200) {
+    console.error('Invalid');
+    return;
+  }
+
+  const films = JSON.parse(body).results;
+  const count = films.reduce((acc, film) => {
+    if (film.characters.some(character => character.includes('/18/'))) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  console.log(count);
+});
