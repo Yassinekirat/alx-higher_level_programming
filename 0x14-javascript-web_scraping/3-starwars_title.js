84261d1c87c4a94a6b81a340c0b2e0e3fb07f44d
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-23 21:49:36 +0100

    task 3 3. Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request.get(url + process.argv[2], function (error, res, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let json = JSON.parse(body);
+    console.log(JSON.parse(body).title);
+  }
+});
