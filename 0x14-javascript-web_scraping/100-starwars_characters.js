fd4f7bb1cb451d2c21e1983c6a99a525382d9190
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-23 22:03:04 +0100

    need to redo these task cuz of short memory loss

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  const fetchCharacterNames = (characterUrls) => {
+    characterUrls.forEach((characterUrl) => {
+      request.get(characterUrl, (error, response, characterBody) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+
+        const characterData = JSON.parse(characterBody);
+        console.log(characterData.name);
+      });
+    });
+  };
+
+  fetchCharacterNames(characters);
+});
