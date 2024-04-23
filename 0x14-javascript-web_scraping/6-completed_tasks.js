1632fe619ce8dfb9edb14a87988e769f49dac166
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-23 21:58:02 +0100

    how many task completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completed = {};
+
+    todos.forEach(todo => {
+      if (todo.completed) {
+        completed[todo.userId] = (completed[todo.userId] || 0) + 1;
+      }
+    });
+
+    console.log(completed);
+  } else {
+    console.error('Error:', error);
+  }
+});
