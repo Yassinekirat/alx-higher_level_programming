1fdab3948357cbdb9a7454f0f7debf7cc1b1457f
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-04-23 22:05:09 +0100

    last task need to work on my js a lot more

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+
+const request = require('request');
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+    printCharacters(characters, 0);
+  } else {
+    console.error(error || 'Invalid request');
+  }
+});
+
+function printCharacters (characters, index) {
+  if (index >= characters.length) return;
+
+  request(characters[index], (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+      printCharacters(characters, index + 1);
+    } else {
+      console.error(error || 'Invalid request');
+    }
+  });
+}
