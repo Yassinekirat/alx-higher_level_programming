38afb5a4678d452714c0fc930ecf5ce32801d23e
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-06-12 12:03:30 +0100

    task 5,6,7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+$(document).ready(function () {
+    $.ajax({
+      type: 'GET',
+      url: 'https://swapi.co/api/people/5/?format=json',
+      success: function (data) {
+        $('DIV#character').text(data.name);
+      }
+    });
+  });
